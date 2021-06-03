bf4e8d734be8880f2f06cb62cba08596e30b1e11
Sayed <nuruddinsayeed@gmail.com>
2021-06-04 03:17:40 +0600

    profileslist and detail endpoint added by viewSets

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from profiles.api.views import ProfileList
+from profiles.api.views import ProfileViewSet
+
+"""the action item here get: list dic must be provied when calling as_view 
+ on a view set"""
+profile_list = ProfileViewSet.as_view({"get": "list"})
+profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
 
 urlpatterns = [
-    path('profiles/', ProfileList.as_view(), name='profile-lis'),
+    path('profileslv/', ProfileList.as_view(), name='profile-lis'),
+
+    path('profiles/', profile_list, name='profile-list'),
+    path('profiles/<int:pk>/', profile_detail, name='profile-detail'),
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
+
+
+class ProfileViewSet(ReadOnlyModelViewSet):
+    """manages profile api endpoints views"""
+
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, ]
+
+# profile list using generics list view
 
 
 class ProfileList(generics.ListAPIView):
